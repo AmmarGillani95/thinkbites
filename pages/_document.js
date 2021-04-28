77d6634c01e211bc3a8b5ca093954148fa409889
Ammar Gillani <ammargillani@Watson.fios-router.home>
2021-04-28 10:44:11 -0400

    added tailwind

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head lang="en" />
+        <body className="bg-white text-black dark:bg-red-500 dark:text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
